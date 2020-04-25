22686dd71055d6020ee9a143acdb6eac83979ab1
Romain Lesur <romain.lesur@gmail.com>
2020-04-25 23:18:29 +0200

    split Spectral into two different dependencies

--- R/fonts.R
+++ R/fonts.R
@@ -1,4 +1,4 @@
-#' HTML dependencies for fonts used by the French government
+#' HTML dependencies for fonts used in French governmental documents
 #'
 #' These functions provide common HTML dependencies for re-use by other R
 #' Markdown output formats or Shiny applications.
@@ -8,11 +8,16 @@
 #' @return An object that can be included in a list of dependencies passed to
 #'   [attachDependencies][htmltools::attachDependencies()].
 #'
-#' @export
 #' @keywords internal
+#' @name fonts-dependencies
 #'
 #' @examples
 #' spectral_font_dep()
+NULL
+
+#' Create an HTML dependency for Spectral font
+#' @rdname fonts-dependencies
+#' @export
 spectral_font_dep <- function(use_gouvdown_fonts = TRUE) {
   if (isTRUE(use_gouvdown_fonts) && xfun::loadable("gouvdown.fonts")) {
     dep <- utils::getFromNamespace("html_dependency_spectral", "gouvdown.fonts")
@@ -20,9 +25,31 @@ spectral_font_dep <- function(use_gouvdown_fonts = TRUE) {
   }
 
   htmltools::htmlDependency(
-    "fontspectral", "v2.000", src = pkg_resource(),
-    head = paste(readLines(pkg_resource("fonts", "spectral", "links.html")), collapse = "\n")
+    "gfontspectral", "v2.000", src = pkg_resource(),
+    head = paste(
+      readLines(pkg_resource("fonts", "spectral", "desktop", "head.html")),
+      collapse = "\n"
+    ),
+    all_files = FALSE
   )
 }
 
+#' Create an HTML dependency for Spectral SmallCaps font
+#' @rdname fonts-dependencies
+#' @export
+spectral_sc_font_dep <- function(use_gouvdown_fonts = TRUE) {
+  if (isTRUE(use_gouvdown_fonts) && xfun::loadable("gouvdown.fonts")) {
+    dep <- utils::getFromNamespace("html_dependency_spectral_sc", "gouvdown.fonts")
+    return(dep())
+  }
+
+  htmltools::htmlDependency(
+    "fontspectral", "v2.000", src = pkg_resource(),
+    head = paste(
+      readLines(pkg_resource("fonts", "spectral", "sc", "head.html")),
+      collapse = "\n"
+    ),
+    all_files = FALSE
+  )
+}
 
