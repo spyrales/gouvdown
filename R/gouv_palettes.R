03a22c7148df2fd2fb2f7a3ccc0fa6a26fc9539b
Romain Lesur <romain.lesur@gmail.com>
2020-04-28 11:52:45 +0200

    dynamically compute the number of palettes

--- R/gouv_palettes.R
+++ R/gouv_palettes.R
@@ -1,5 +1,5 @@
 #' Colors palettes for France design system
 #'
-#' @format A named list with 23 palettes.
+#' @format A named list with `r length(gouvdown::gouv_palettes)` palettes.
 #' @inherit gouv_colors references
 "gouv_palettes"
