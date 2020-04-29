a8481524f45e43443a33581387779dcd403f4770
Maël Theuliere <mael.theuliere@developpement-durable.gouv.fr>
2020-04-29 08:29:37 +0200

    add theme_gouv

--- R/ggthemes.R
+++ R/ggthemes.R
@@ -89,3 +89,148 @@ check_fonts_in_r <- function(fonts = c("Marianne", "Spectral"),
   res
 }
 
+
+#' ggplot2 theme with french government design template
+#'
+#' @md
+#' @param base_family,base_size base font family and size
+#' @param plot_title_family,plot_title_face,plot_title_size,plot_title_margin plot title family, face, size and margi
+#' @param subtitle_family,subtitle_face,subtitle_size plot subtitle family, face and size
+#' @param subtitle_margin plot subtitle margin bottom (single numeric value)
+#' @param strip_text_family,strip_text_face,strip_text_size facet label font family, face and size
+#' @param caption_family,caption_face,caption_size,caption_margin plot caption family, face, size and margin
+#' @param axis_title_family,axis_title_face,axis_title_size axis title font family, face and size
+#' @param axis_title_just axis title font justification, one of `[blmcrt]`
+#' @param plot_margin plot margin (specify with `ggplot2::margin()`)
+#' @param grid_col,axis_col grid & axis colors; both default to `white`
+#' @param grid panel grid (`TRUE`, `FALSE`, or a combination of `X`, `x`, `Y`, `y`)
+#' @param panel_background_color panel background fill color; defaut to `#C0C0C0`
+#' @param panel_border_color panel border color; defaut to `888888`
+#' @param legend_background_color legend background fill color; defaut to `#C0C0C0`
+#' @param legend_border_color legend border color; defaut to `888888`
+#' @param axis_text_size font size of axis text
+#' @param axis add x or y axes? `TRUE`, `FALSE`, "`xy`"
+#' @param ticks ticks if `TRUE` add ticks
+#'
+#' @importFrom ggplot2 element_blank element_line element_text margin theme element_rect
+#' @return a ggplot2 theme
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' library(ggplot2)
+#' ggplot(mtcars) +
+#'   geom_point(aes(cyl, mpg)) +
+#'   labs(title = "Mon titre avec Marianne",
+#'        x = "lab X avec Spectral",
+#'        y = "Lab Y avec Spectral") +
+#'   theme_gouv()
+#' }
+
+
+theme_gouv <- function(base_family="Spectral", base_size = 12,
+                       plot_title_family="Marianne", plot_title_size = 28,
+                       plot_title_face="bold", plot_title_margin = 10,
+                       subtitle_family="Marianne Light", subtitle_size = 22,
+                       subtitle_face = "plain", subtitle_margin = 9,
+                       strip_text_family = base_family, strip_text_size = 22,
+                       strip_text_face = "plain",
+                       caption_family = base_family, caption_size = 12,
+                       caption_face = "plain", caption_margin = 10,
+                       axis_text_size = base_size,
+                       axis_title_family = subtitle_family, axis_title_size = base_size,
+                       axis_title_face = "plain", axis_title_just = "rt",
+                       plot_margin = margin(30, 30, 30, 30),
+                       grid_col = "#ffffff", grid = TRUE,
+                       panel_background_color = "#C0C0C0",
+                       panel_border_color = "#888888",
+                       legend_background_color = "#C0C0C0",
+                       legend_border_color = "#888888",
+                       axis_col = "#cccccc", axis = FALSE, ticks = FALSE) {
+
+  ret <- ggplot2::theme_minimal(base_family=base_family, base_size=base_size)
+
+  ret <- ret + theme(legend.background = element_rect(fill = legend_background_color,
+                                                    color = legend_border_color))
+  ret <- ret + theme(legend.key=element_blank())
+
+  if (inherits(grid, "character") | grid == TRUE) {
+
+    ret <- ret + theme(panel.grid=element_line(color=grid_col, size=0.2,linetype = "dotted"))
+    ret <- ret + theme(panel.grid.major=element_line(color=grid_col, size=0.2,linetype = "dotted"))
+    ret <- ret + theme(panel.grid.minor=element_line(color=grid_col, size=0.15,linetype = "dotted"))
+    ret <- ret + theme(panel.background = element_rect(fill=panel_background_color, color = panel_border_color, size=0.15))
+
+    if (inherits(grid, "character")) {
+      if (regexpr("X", grid)[1] < 0) ret <- ret + theme(panel.grid.major.x=element_blank())
+      if (regexpr("Y", grid)[1] < 0) ret <- ret + theme(panel.grid.major.y=element_blank())
+      if (regexpr("x", grid)[1] < 0) ret <- ret + theme(panel.grid.minor.x=element_blank())
+      if (regexpr("y", grid)[1] < 0) ret <- ret + theme(panel.grid.minor.y=element_blank())
+    }
+
+  } else {
+    ret <- ret + theme(panel.grid=element_blank())
+  }
+
+  if (inherits(axis, "character") | axis == TRUE) {
+    ret <- ret + theme(axis.line=element_line(color="#2b2b2b", size=0.15))
+    if (inherits(axis, "character")) {
+      axis <- tolower(axis)
+      if (regexpr("x", axis)[1] < 0) {
+        ret <- ret + theme(axis.line.x=element_blank())
+      } else {
+        ret <- ret + theme(axis.line.x=element_line(color=axis_col, size=0.15))
+      }
+      if (regexpr("y", axis)[1] < 0) {
+        ret <- ret + theme(axis.line.y=element_blank())
+      } else {
+        ret <- ret + theme(axis.line.y=element_line(color=axis_col, size=0.15))
+      }
+    } else {
+      ret <- ret + theme(axis.line.x=element_line(color=axis_col, size=0.15))
+      ret <- ret + theme(axis.line.y=element_line(color=axis_col, size=0.15))
+    }
+  } else {
+    ret <- ret + theme(axis.line=element_blank())
+  }
+
+  if (!ticks) {
+    ret <- ret + theme(axis.ticks = element_blank())
+    ret <- ret + theme(axis.ticks.x = element_blank())
+    ret <- ret + theme(axis.ticks.y = element_blank())
+  } else {
+    ret <- ret + theme(axis.ticks = element_line(size=0.15))
+    ret <- ret + theme(axis.ticks.x = element_line(size=0.15))
+    ret <- ret + theme(axis.ticks.y = element_line(size=0.15))
+    ret <- ret + theme(axis.ticks.length = grid::unit(5, "pt"))
+  }
+
+  xj <- switch(tolower(substr(axis_title_just, 1, 1)), b=0, l=0, m=0.5, c=0.5, r=1, t=1)
+  yj <- switch(tolower(substr(axis_title_just, 2, 2)), b=0, l=0, m=0.5, c=0.5, r=1, t=1)
+
+  ret <- ret + theme(axis.text.x=element_text(size=axis_text_size, margin=margin(t=0)))
+  ret <- ret + theme(axis.text.y=element_text(size=axis_text_size, margin=margin(r=0)))
+  ret <- ret + theme(axis.title=element_text(size=axis_title_size, family=axis_title_family))
+  ret <- ret + theme(axis.title.x=element_text(hjust=xj, size=axis_title_size,
+                                               family=axis_title_family, face=axis_title_face))
+  ret <- ret + theme(axis.title.y=element_text(hjust=yj, size=axis_title_size,
+                                               family=axis_title_family, face=axis_title_face))
+  ret <- ret + theme(axis.title.y.right=element_text(hjust=yj, size=axis_title_size, angle=90,
+                                                     family=axis_title_family, face=axis_title_face))
+  ret <- ret + theme(strip.text=element_text(hjust=0, size=strip_text_size,
+                                             face=strip_text_face, family=strip_text_family))
+  ret <- ret + theme(panel.spacing=grid::unit(2, "lines"))
+  ret <- ret + theme(plot.title=element_text(hjust=0, size=plot_title_size,
+                                             margin=margin(b=plot_title_margin),
+                                             family=plot_title_family, face=plot_title_face))
+  ret <- ret + theme(plot.subtitle=element_text(hjust=0, size=subtitle_size,
+                                                margin=margin(b=subtitle_margin),
+                                                family=subtitle_family, face=subtitle_face))
+  ret <- ret + theme(plot.title.position="plot")
+  ret <- ret + theme(plot.caption=element_text(hjust=1, size=caption_size,
+                                               margin=margin(t=caption_margin),
+                                               family=caption_family, face=caption_face))
+  ret <- ret + theme(plot.margin=plot_margin)
+
+  ret
+}
