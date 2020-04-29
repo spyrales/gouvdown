7aed7c9d2539fadd5100c0acb32fd94e240585b2
Maël Theuliere <mael.theuliere@developpement-durable.gouv.fr>
2020-04-29 08:29:18 +0200

    typo
    modify onLoad message and check font in r exemple

--- R/onLoad.R
+++ R/onLoad.R
@@ -4,7 +4,7 @@
   packageStartupMessage("\n\n*******************************************************")
   # packageStartupMessage("Note: gouvdown changes the default ggplot2 theme.")
   # packageStartupMessage("To recover the default ggplot2 theme, execute:\n  theme_set(theme_gray())")
-  packageStartupMessage("theme_gouv() for ggplot2 is not set as default,\nto do so, execute:\n  theme_set(theme_dreal())")
+  packageStartupMessage("theme_gouv() for ggplot2 is not set as default,\nto do so, execute:\n  theme_set(theme_gouv())")
   packageStartupMessage("*******************************************************\n")
 }
 .onLoad <- function(libname, pkgname) {
