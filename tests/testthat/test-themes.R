c1c5cbdc79f43999af3d8690faa887e3ec0c7d48
Maël Theuliere <mael.theuliere@developpement-durable.gouv.fr>
2020-05-07 08:07:58 +0200

    Fix tests style
    style `test-logos.R` and `test-themes.R`

--- tests/testthat/test-themes.R
+++ tests/testthat/test-themes.R
@@ -1,8 +1,10 @@
-test_that("theme_gouv() is a theme",
-          expect_is(theme_gouv(), "theme")
+test_that(
+  "theme_gouv() is a theme",
+  expect_is(theme_gouv(), "theme")
 )
 
 
-test_that("theme_grey_gouv() is a theme",
-          expect_is(theme_gouv(), "theme")
+test_that(
+  "theme_grey_gouv() is a theme",
+  expect_is(theme_gouv(), "theme")
 )
