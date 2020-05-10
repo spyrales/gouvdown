32c3a1b3febc72a8d62911418634f247d4beb765
Maël Theuliere <mael.theuliere@developpement-durable.gouv.fr>
2020-05-11 01:28:59 +0200

    add test for theme_gouv_map()
    - new test for theme_gouv_map()
    - bug fix on theme_grey_gouv() test

--- tests/testthat/test-themes.R
+++ tests/testthat/test-themes.R
@@ -3,8 +3,13 @@ test_that(
   expect_is(theme_gouv(), "theme")
 )
 
-
 test_that(
   "theme_grey_gouv() is a theme",
-  expect_is(theme_gouv(), "theme")
+  expect_is(theme_grey_gouv(), "theme")
 )
+
+test_that(
+  "theme_gouv_map() is a theme",
+  expect_is(theme_gouv_map(), "theme")
+)
+
