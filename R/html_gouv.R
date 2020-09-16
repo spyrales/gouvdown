ae1f17e82970ee9ab386ed1bedf40c27fd82ce79
Maël THEULIERE <mael.theuliere@gmail.com>
2020-09-16 09:50:03 +0200

    Markdown (#21)
    * add html_gouv() rmarkdown template
    
    * add inst/resources/css/default.css : theming toc, author, date and title
    
    * Add test for rmarkdown template
    
    * update NEWS.md

---
+++ R/html_gouv.R
@@ -0,0 +1,72 @@
+#' create html_gouv header
+#'
+#' @param file Path to a `png` file.
+#' @param logo Name of a logo available in `gouvdown`. The list of available
+#'   logos can be obtained with [list_logos()].
+#' @param output name of the header. The logo is copied in an 'www' subfolder
+#' @importFrom glue glue
+#' @importFrom here here
+#'
+#' @return an html file
+#' @export
+#'
+#' @examples
+#' create_header_html_gouv(logo = 'gouvernement')
+create_header_html_gouv <- function(logo = NULL, file = logo_file_path(logo), output = 'header.html') {
+  if (!xor(missing(logo), missing(file))) {
+    stop("use either a logo name or a path to a file")
+  }
+
+  if (!missing(logo)) {
+    match.arg(logo, list_logos())
+  }
+
+  if (length(file) > 1) {
+    stop("please select only one file")
+  }
+  logo_name <- basename(file)
+  if (!dir.exists(here::here("www"))) {dir.create(here::here("www"))}
+  file.copy(file,'www')
+  writeLines(
+    glue(
+      "<div class=\"row\">
+
+<div class=\"col-md-4\">
+<a>
+<img src=\"www/{logo_name}\" width=\"215px\">
+</a>
+</div>
+
+</div>"),
+    output
+  )
+}
+
+#' gouvdown html document
+#'
+#' @param ... Additional arguments passed to \code{rmarkdown::\link{html_document}()}.
+#' @param extra_dependencies  extra dep
+#' @param css css
+#' @param header header to include
+#' @return An R Markdown output format object to be passed to
+#'   \code{rmarkdown::\link{render}()}.
+#' @importFrom rmarkdown includes
+#' @export
+html_gouv = function(..., extra_dependencies = list(),css = NULL,header = 'header.html') {
+  default_css <- pkg_resource('css','default.css')
+  if (xfun::loadable("gouvdown.fonts")) {
+  extra_deps <- append(extra_dependencies,
+                               list(
+                                 marianne_font_dep(),
+                                 spectral_font_dep(),
+                                 spectral_sc_font_dep()
+                                 )
+  )
+  }
+  else {
+    extra_deps <- append(extra_dependencies)
+  }
+  rmarkdown::html_document(..., extra_dependencies = extra_deps,
+  css = c(default_css,css),
+  includes = includes(before_body = header))
+}
---
+++ tests/testthat/test-rmarkdown.R
@@ -0,0 +1,20 @@
+# inspired by mitchell O'Hara-Wild vitae package : https://github.com/mitchelloharawild/vitae/blob/master/tests/testthat/test-template.R
+expect_render <- function(template){
+
+  outputdir <- tempfile()
+  dir.create(outputdir)
+  dir.create(file.path(outputdir,'www'))
+  file.copy(system.file("rmarkdown", "templates", template, "skeleton", "skeleton.Rmd", package = "gouvdown"),outputdir)
+  file <- file.path(outputdir,"skeleton.Rmd")
+  expect_output(
+    expect_message(
+      rmarkdown::render(
+        file,
+        output_dir = outputdir),
+      "Output created"),
+    "pandoc")
+}
+
+test_that("gouvdown_html_document", {
+  expect_render("gouvdown_html_document")
+})
