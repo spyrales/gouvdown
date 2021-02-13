ac32e2a0eb6f3afd800c959ff227a75dc7696c4a
Thomas Vroylandt <tvroylandt@gmail.com>
2021-02-13 15:57:03 +0100

    Ajout template Pandoc a blanc

--- R/propre_brochure.R
+++ R/propre_brochure.R
@@ -10,14 +10,16 @@ propre_brochure <- function(...,
                             css = NULL) {
 
   # custom css
-  book_css <- pkg_resource("css/propre/book.css")
+  book_css <- pkg_resource("css/propre", "book.css")
 
   # custom Pandoc template
+  brochure_html <- pkg_resource("html", "propre_brochure.html")
 
   # render
   pagedown::html_paged(number_sections = FALSE,
                        # self_contained = FALSE,
                        toc = FALSE,
                        css = c(book_css, css),
+                       template = brochure_html,
                        ...)
 }
