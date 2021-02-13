df89c7819fd18de04c323ca2828e7127c3711cb8
Thomas Vroylandt <tvroylandt@gmail.com>
2021-02-13 17:10:27 +0100

    Gestion blocs marques

--- R/propre_brochure.R
+++ R/propre_brochure.R
@@ -1,16 +1,34 @@
 #' gouvdown PDF brochure - PROPRE style
 #'
+#' @inheritParams html_gouv
 #' @param ... Additional arguments passed to \code{bookdown::\link{html_document2}()}.
 #' @param css A character vector of additionnal CSS file paths.
-#' @param use_gouvdown_fonts Use the fonts provided by the `gouvdown.fonts` package, if installed.
 #'
 #' @return An R Markdown output format object to be passed to
 #'   \code{rmarkdown::\link{render}()}.
 #' @export
 propre_brochure <- function(extra_dependencies = list(),
+                            includes = NULL,
                             ...,
+                            logo = NULL,
                             css = NULL,
                             use_gouvdown_fonts = TRUE) {
+  # init variable
+  logo_html_fragment <- NULL
+
+  # create HTML fragment for logo
+  if (!is.null(logo)){
+    if (xfun::file_ext(logo) == "") {
+      logo_html_fragment <- create_header_html_gouv(logo = logo)
+    }
+    else {
+      match.arg(xfun::file_ext(logo), c("png", "svg", "jpg", "gif"))
+      logo_html_fragment <- create_header_html_gouv(file = logo)
+    }
+  }
+
+  includes <- as.list(includes)
+  includes$before_body <- c(includes$before_body, logo_html_fragment)
 
   # custom css
   book_css <- pkg_resource("css/propre", "book.css")
@@ -26,6 +44,7 @@ propre_brochure <- function(extra_dependencies = list(),
 
   # render
   pagedown::html_paged(extra_dependencies = extra_dependencies,
+                       includes = includes,
                        number_sections = FALSE,
                        # self_contained = FALSE,
                        toc = FALSE,
