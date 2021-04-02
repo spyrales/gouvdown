a7e896b9205aa97f7c4e57637aea398e1aa9cf06
Romain Lesur <romain.lesur@gmail.com>
2021-04-03 00:10:23 +0200

    do not pollute the global environment

--- dev/run_dev_html_gouv.R
+++ dev/run_dev_html_gouv.R
@@ -1,16 +1,17 @@
 ## Script pour les modifs en live
+local({
+  all_attached <- paste("package:", names(sessionInfo()$otherPkgs),
+                        sep = "")
+  try(suppressWarnings(lapply(all_attached,
+                              detach, character.only = TRUE, unload = TRUE)), silent = TRUE)
 
-all_attached <- paste("package:", names(sessionInfo()$otherPkgs),
-                      sep = "")
-try(suppressWarnings(lapply(all_attached,
-       detach, character.only = TRUE, unload = TRUE)), silent = TRUE)
+  pkgload::load_all()
 
-pkgload::load_all()
+  file_rmd <- system.file("rmarkdown", "templates", "gouvdown_html_document", "skeleton", "skeleton.Rmd", package = "gouvdown")
 
-file_rmd <- system.file("rmarkdown", "templates", "gouvdown_html_document", "skeleton", "skeleton.Rmd", package = "gouvdown")
-
-ok <- rmarkdown::render(
-  file_rmd,
-  output_format = gouvdown::html_gouv()
-)
-browseURL(ok)
+  ok <- rmarkdown::render(
+    file_rmd,
+    output_format = gouvdown::html_gouv()
+  )
+  browseURL(ok)
+})
